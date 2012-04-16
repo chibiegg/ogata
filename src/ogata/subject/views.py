6d503c286b9755a8ff69b6aaffe1745495bcc3b7
chibiegg <chibiegg@chibiegg.net>
2012-04-16 11:38:12 +0900

    登録後、ログインされた状態に。

--- src/ogata/subject/views.py
+++ src/ogata/subject/views.py
@@ -3,6 +3,8 @@ from ogata.subject.forms import RegistrationForm
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views.generic.simple import direct_to_template
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 
 
 def registration(request):
@@ -12,13 +14,16 @@ def registration(request):
     if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            #ログインされた状態にする
+            user = authenticate(username=user.username,password=form.cleaned_data["passwd"])
+            login(request, user)
             return HttpResponseRedirect(reverse("regist_complete"))
     
     context = {"form":form}
     return direct_to_template(request, "subject/regist.html", context)
 
-
+@login_required
 def registration_complete(request):
     """新規登録完了"""
     return direct_to_template(request, "subject/regist_complete.html")
